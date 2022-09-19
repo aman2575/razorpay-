4b2c75f0b0978e4304ad31fd8b09aa5639a0e785
AMAN <g1aman.singh2575@gmail.com>
2022-09-19 11:04:10 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [amount, setAmount] = useState('');
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    if(amount === ""){
+      alert('please enter amount');
+    }else{
+      var options = {
+        key:'rzp_test_LvYxWSL0ndCMso',
+        key_secret:'2hzmX6zM6SsX6sxkDm5jyAaY',
+        amount: amount *100,
+        currency:'INR',
+        name:"AMAN_ACCOUNT",
+        description:"for testing purpose",
+        handler: function(res){
+          alert(res.razorpay_payment_id);
+        },
+        prefill: {
+          name:"AMAN SINGH",
+          email:"g1aman.singh2575@gmail.com",
+          contact: '9529362741'
+        },
+        notes:{
+          address: "Razorpay Corporate office"
+        },
+        theme: {
+          color: '#3399cc'
+        }
+      };
+      var pay = new window.Razorpay(options);
+      pay.open();
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Razorpay Payment Integration using react</h2>
+      <br />
+      <input type='number' placeholder='Enter Amount' value={amount} 
+      onChange={(e) => setAmount(e.target.value)} />
+      <br />
+      <button onClick={handlesubmit}>submit</button>
+      <br />
     </div>
   );
 }
